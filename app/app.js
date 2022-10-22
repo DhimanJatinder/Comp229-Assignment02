a257dc23e66e5f0cde1ccc30fecac8e29ba0a1c7
Jatinder Dhiman <jdhiman1@my.centennialcollege.ca>
2022-10-22 19:30:15 -0400

    Mongoose And Movies Model Completed

--- app/app.js
+++ app/app.js
@@ -10,14 +10,25 @@ import { fileURLToPath } from "url";
 import exp from "constants";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// Import Mongoose Module
+import mongoose from "mongoose";
+
 // Configuration Module
-import { Secret } from "../config/config.js";
+import { Secret, MongoURI } from "../config/config.js";
 
 // Import Router
 import indexRouter from "./routes/index.route.server.js";
 
 //instantiate app server
 const app = express();
+
+//Complete the DB Configuration
+mongoose.connect(MongoURI);
+const db = mongoose.connection;
+
+//Listen for connection success or error
+db.on('open', () => console.log("Connected to MongoDB"));
+db.on('error', () => console.log("Mongo Connection Error"));
 
 // setup ViewEngine EJS
 app.set("views", path.join(__dirname, "/views"));
