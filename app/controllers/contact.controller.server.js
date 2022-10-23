a5da87ea2284770da735bdc22bd6434d03e82e4b
Jatinder Dhiman <jdhiman1@my.centennialcollege.ca>
2022-10-22 23:47:13 -0400

    Add Edit Delete Working

--- app/controllers/contact.controller.server.js
+++ app/controllers/contact.controller.server.js
@@ -10,7 +10,68 @@ export function DisplayContactsList(req, res, next){
         res.render('index', {title: 'Contact List', page: 'contacts/list', contacts: contactsCollection});
     })
 }
-
 export function DisplayContactsAddPage(req, res, next){
     res.render('index', { title: "Add Contact", page: "contacts/edit", contact: {} });
+}
+export function ProcessContactsAddPage(req, res, next){
+ 
+    let newContact = contactsModel({
+        name: req.body.name,
+        contact: req.body.contact,
+        email: req.body.email
+    });
+
+    contactsModel.create(newContact, (err, Contact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.redirect('/contact-list')
+    })
+}
+export function DisplayContactsEditPage(req, res, next){
+
+    let id = req.params.id ;
+
+    contactsModel.findById(id, (err, contact) =>{
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.render('index', {title: 'Edit Contact', page: 'contacts/edit', contact: contact });
+    });
+}
+
+export function ProcessContactsEditPage(req, res, next){
+
+    let newContact = contactsModel({
+        _id:req.body.id,
+        name: req.body.name,
+        contact: req.body.contact,
+        email: req.body.email
+    });
+
+    contactsModel.updateOne({_id:req.params.id} ,newContact, (err, Contact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.redirect('/contact-list')
+    })
+
+}
+export function ProcessContactsDelete(req, res, next){
+    let id = req.params.id;
+
+    contactsModel.remove({_id:id}, (err) =>{
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.redirect('/contact-list');
+    })
 }
--- app/routes/contacts.route.server.js
+++ app/routes/contacts.route.server.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 
-import { DisplayContactsAddPage, DisplayContactsList } from "../controllers/contact.controller.server.js";
+import { DisplayContactsList, DisplayContactsAddPage, ProcessContactsAddPage, DisplayContactsEditPage, ProcessContactsEditPage ,ProcessContactsDelete } from "../controllers/contact.controller.server.js";
 
 const router = Router();
 router.get('/contact-list', DisplayContactsList);
 router.get('/contact-add', DisplayContactsAddPage);
+router.post('/contact-add', ProcessContactsAddPage);
+router.get('/contact-edit/:id', DisplayContactsEditPage );
+router.post('/contact-edit/:id',ProcessContactsEditPage);
+router.get('/contact-delete/:id', ProcessContactsDelete);
 
 export default router ;
