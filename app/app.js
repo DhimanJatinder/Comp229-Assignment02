f7c315a9ba358569e624a5687a986321cd7d8c13
Jatinder Dhiman <jdhiman1@my.centennialcollege.ca>
2022-10-23 02:28:13 -0400

    Passport Module Added and Login Register pages created

--- app/app.js
+++ app/app.js
@@ -10,6 +10,17 @@ import { fileURLToPath } from "url";
 import exp from "constants";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// Auth Step-1 Import Modules
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import flash from 'connect-flash';
+
+// Auth Step-2 define auth strategy
+let localStrategy = passportLocal.Strategy;
+
+// Auth Step-3 import the user model
+import User from './models/user.js';
+
 // Import Mongoose Module
 import mongoose from "mongoose";
 
@@ -40,6 +51,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
+
+// Auth Step-4- Setup Express Session
 app.use(
   session({
     secret: Secret,
@@ -47,6 +60,20 @@ app.use(
     resave: false,
   })
 );
+
+// Auth Step-5 Setup Flash messages
+app.use(flash());
+
+// Auth Step-6 Init passport and session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Auth Step-7 Implement the Auth Strategy
+passport.use(User.createStrategy());
+
+// Auth Step-8 Setup serialization and deerialization
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Use Routes
 app.use("/", indexRouter);
